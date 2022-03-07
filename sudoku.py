3fafbf937e9d3fead8cc927bc5a7f4516b0bdac8
Tian Zhou <70028519+Tzeriz@users.noreply.github.com>
2022-03-07 13:17:27 -0800

    Add entering feature, refract variables - 3/4/2022
    - Add reset() function correspond to "clear" button in main.py
    - Add enter() function to enter number to the tile
    - Delete class Tile
    - Add local variables to Sudoku class: focused tile, color variables from class Tile, new valid input number left list

--- sudoku.py
+++ sudoku.py
@@ -1,11 +1,28 @@
 # import pygame
 
-tile_color_list = ["#ffffff", "#cccccc", "#ffcccb"]
+tile_color_list = ["#ffffff", "#cccccc", "#ffcccb"]  # white, light-gray, light-red
+text_color_list = ["#000000", "#ff0000", "#0000ff"]  # black, red, blue
 
 class Sudoku:
 
     def __init__(self):
-        self.grid = [[Tile() for _ in range(9)] for _ in range(9)]
+        self.num = [[None for _ in range(9)] for _ in range(9)]
+        self.curr_focus = (-1, -1)
+        self.valid = [[[1, 2, 3, 4, 5, 6, 7, 8, 9]] * 9] * 9  # valid input list for 9 * 9 grid
+        # graphic
+        self.tile_color = [[tile_color_list[0] for _ in range(9)] for _ in range(9)]
+        self.text_color = [[text_color_list[0] for _ in range(9)] for _ in range(9)]
+
+    def reset(self):
+        self.num = [[None for _ in range(9)] for _ in range(9)]
+        self.curr_focus = (-1, -1)
+        self.valid = [[[1, 2, 3, 4, 5, 6, 7, 8, 9]] * 9] * 9
+        self.tile_color = [[tile_color_list[0] for _ in range(9)] for _ in range(9)]
+        self.text_color = [[text_color_list[0] for _ in range(9)] for _ in range(9)]
+
+    def enter(self, num_input):
+        if self.curr_focus != (-1, -1):
+            self.num[self.curr_focus[0]][self.curr_focus[1]] = num_input
 
     def check(self):
         pass
@@ -14,12 +31,8 @@ class Sudoku:
         for i in range(9):
             print("[", end="")
             for k in range(8):
-                print(self.grid[i][k], end=", ")
-            print(self.grid[i][8], "]", sep="")
+                print(self.num[i][k], end=", ")
+            print(self.num[i][8], "]", sep="")
 
-
-class Tile:
-
-    def __init__(self):
-        self.num = 0
-        self.color = tile_color_list[0]
+    def solve(self):
+        pass
