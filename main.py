76e1215684e31d359a03137ac0f8b0a3f3906614
Tian Zhou <70028519+Tzeriz@users.noreply.github.com>
2022-03-08 00:09:27 -0800

    Minor color edits - 3/7/2022 - Fix text_color bug (cause: variable naming, syntax error) - Edit import sudoku statement - Edit color variables reference - now solely store in sudoku.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
 import pygame
 
-from sudoku import *
+import sudoku
+
 
 """ Pygame Setup and Global Variables"""
 pygame.init()
-screen = pygame.display.set_mode((390, 600))
+screen = pygame.display.set_mode((400, 600))
 # Preset size = (390, 600)
 pygame.display.set_caption("Sudoku Solver")
 scr = [screen.get_size()[0] - 2, screen.get_size()[1] - 27]
@@ -13,7 +14,6 @@ clock = pygame.time.Clock()
 pygame.time.set_timer(pygame.USEREVENT, 100)
 
 """ Graphic Global Variables """
-background_color, line_color, text_color = (255, 255, 255), (0, 0, 0), (0, 0, 0)
 # tile global var
 tile_size = scr[0] // 11
 grid_row, grid_col = [], []
@@ -25,19 +25,18 @@ num_x = [(scr[0] - 9 * num_rect_size) / 2]  # -1 is for adjusting alignment with
 button_rect = []
 button_size = (scr[0] // 5.5, scr[1] // 18)
 
-""" UI Graphics """
 
-
+# UI Graphics
 def draw(curr_sudoku):
     global screen, scr, tile_size, grid_row, grid_col, num_rect_size, num_x, num_y, button_size, button_rect
 
-    screen.fill(background_color)  # UI background
+    screen.fill(sudoku.tile_color_list[0])  # UI background
 
     """ Title """
 
     title_rect = [scr[0] / 4, scr[1] * 0.07, scr[0] / 2, scr[1] * 0.07]
     title_font = pygame.font.SysFont("Helvetica", scr[0] // 13)
-    text_render = title_font.render("Sudoku Solver", True, text_color)
+    text_render = title_font.render("Sudoku Solver", True, sudoku.text_color_list[0])
     text_rect = text_render.get_rect(center=(title_rect[0] + title_rect[2] / 2, title_rect[1] + title_rect[3] / 2))
     screen.blit(text_render, text_rect)
 
@@ -50,7 +49,7 @@ def draw(curr_sudoku):
     font = pygame.font.SysFont("Helvetica", int(tile_size / 1.5))
 
     # Draw Grid
-    pygame.draw.rect(screen, line_color, grid_rect)  # grid background (border)
+    pygame.draw.rect(screen, sudoku.text_color_list[0], grid_rect)  # grid background (border)
 
     # Draw Tile
     for i in range(9):  # row
@@ -66,7 +65,7 @@ def draw(curr_sudoku):
             # draw Tile
             tile_rect = [grid_col[k], grid_row[i], tile_size, tile_size]
             if curr_sudoku.curr_focus == (i, k):
-                pygame.draw.rect(screen, "#cccccc", tile_rect)  # focus tile - gray
+                pygame.draw.rect(screen, sudoku.tile_color_list[1], tile_rect)  # focus tile - gray
             else:
                 pygame.draw.rect(screen, curr_sudoku.tile_color[i][k], tile_rect)
 
@@ -86,7 +85,7 @@ def draw(curr_sudoku):
 
     for i in range(9):
         num_rect = [num_x[i], num_y, num_rect_size, num_rect_size]
-        num_render = num_font.render(str(i + 1), True, text_color)
+        num_render = num_font.render(str(i + 1), True, sudoku.text_color_list[0])
         num_text_rect = num_render.get_rect(center=(num_rect[0] + num_rect[2] // 2, num_rect[1] + num_rect[3] / 2))
         screen.blit(num_render, num_text_rect)
 
@@ -100,11 +99,12 @@ def draw(curr_sudoku):
         button_rect.append([(scr[0] - 3 * button_size[0] - 2 * button_space) / 2 + i * (button_size[0] + button_space), scr[1] * 8/9, button_size[0], button_size[1]])
         pygame.draw.rect(screen, "#cccccc", button_rect[i])
         # blit button text
-        button_render = button_font.render(button_text[i], True, text_color)
+        button_render = button_font.render(button_text[i], True, sudoku.text_color_list[0])
         button_text_rect = button_render.get_rect(center=(button_rect[i][0] + button_rect[i][2] // 2, button_rect[i][1] + button_rect[i][3] / 2))
         screen.blit(button_render, button_text_rect)
 
 
+# Check if a tile is clicked - Focus
 def click_grid(curr_sudoku, curr_mouse):
     # Return: True if a tile is been clicked
     global grid_row, grid_col, tile_size
@@ -120,6 +120,7 @@ def click_grid(curr_sudoku, curr_mouse):
     return False
 
 
+# Check if an number option is been clicked - Input
 def click_num(curr_sudoku, curr_mouse):
     # Return: True if a num tile has been clicked
     global num_rect_size, num_x, num_y
@@ -132,6 +133,8 @@ def click_num(curr_sudoku, curr_mouse):
                 return True
     return False
 
+
+# Check if a button has been clicked - Function (delete, clear, and solve)
 def click_button(curr_sudoku, curr_mouse):
     global button_size, button_rect
 
@@ -143,20 +146,21 @@ def click_button(curr_sudoku, curr_mouse):
                 curr_sudoku.reset()
             elif i == 2:
                 curr_sudoku.solve()
+            return True
+    return False
 
-""" main() """
 
-
+# MAIN()
+# Function Loop
 def main():
     """ Sudoku Setup """
-    sudoku = Sudoku()
+    solver = sudoku.Sudoku()
 
     """ Function Loop"""
-    end = False
-
-    draw(sudoku)
+    draw(solver)
     pygame.display.update()
 
+    end = False
     while not end:
 
         for event in pygame.event.get():
@@ -167,8 +171,8 @@ def main():
 
             if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:
                 mouse = pygame.mouse.get_pos()
-                if click_grid(sudoku, mouse) or click_num(sudoku, mouse) or click_button(sudoku, mouse):
-                    draw(sudoku)
+                if click_grid(solver, mouse) or click_num(solver, mouse) or click_button(solver, mouse):
+                    draw(solver)
                     pygame.display.update()
 
         # Draw
