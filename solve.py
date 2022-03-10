7b0dd0c1221bd0a0df140a3670e81db2593e39c7
Tian Zhou <70028519+Tzeriz@users.noreply.github.com>
2022-03-10 13:47:37 -0800

    solve() prototype and unit_test set up - 3/10/2022
    - Expand solve() for 4 * 4 grid to 9 * 9
    - Create solve_unittest.py and sample sudoku problems

--- solve.py
+++ solve.py
@@ -1,7 +1,5 @@
 problem = []
 
-
-# Try and continue approach
 """
 search black -> row, col
 for i:
@@ -10,55 +8,37 @@ delete()
 return false
 """
 
-def solve_4():
+def solve():
     global problem
 
     # check next block
     pos = None
-    for i in range(16):
-        if problem[i // 4][i % 4] == 0:
-            pos = (i // 4, i % 4)
+    for i in range(81):
+        if problem[i // 9][i % 9] == 0:
+            pos = (i // 9, i % 9)
             break
     if pos is None:
         return True
 
     # check possible output
-    possible = [i for i in range(1, 5)]
-    for i in range(4):
+    possible = [i for i in range(1, 10)]
+    for i in range(9):
         # same row
         if (pos[1] != i) and problem[pos[0]][i] in possible:
             possible.remove(problem[pos[0]][i])
         # same col
         if (pos[0] != i) and problem[i][pos[1]] in possible:
             possible.remove(problem[i][pos[1]])
-    # check 2 * 2 grid
-    for row_del in range(pos[0] // 2 * 2, pos[0] // 2 * 2 + 2):
-        for col_del in range((pos[1] % 4) // 2 * 2, (pos[1] % 4) // 2 * 2 + 2):
+    # check 3 * 3 grid
+    for row_del in range(pos[0] // 3 * 3, pos[0] // 3 * 3 + 3):
+        for col_del in range((pos[1] % 9) // 3 * 3, (pos[1] % 9) // 3 * 3 + 3):
             if (row_del != pos[0]) and (col_del != pos[1]) and (problem[row_del][col_del] in possible):
                 possible.remove(problem[row_del][col_del])
 
     # recursion
     for num in possible:
         problem[pos[0]][pos[1]] = num
-        if solve_4():
+        if solve():
             return True
     problem[pos[0]][pos[1]] = 0
     return False
-
-
-def main():
-    global problem
-
-    # Test
-    # problem_text = "4..331..2..1..2."
-    problem_text = "." * 16
-    problem = [[0 for _ in range(4)] for _ in range(4)]
-    for i in range(16):
-        problem[i // 4][i % 4] = 0 if problem_text[i] == "." else int(problem_text[i])
-
-    print(solve_4())
-    print(problem)
-
-
-if __name__ == '__main__':
-    main()
